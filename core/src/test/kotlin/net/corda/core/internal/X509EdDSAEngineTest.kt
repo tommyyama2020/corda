9a3a0e4ceebac93cd3afddacb292ec5ff2fce2e7
Christian Sailer <christian.sailer@r3.com>
2019-09-06 15:56:08 +0100

    Reinstate expected failure for the old X509Engine

--- core/src/test/kotlin/net/corda/core/internal/X509EdDSAEngineTest.kt
+++ core/src/test/kotlin/net/corda/core/internal/X509EdDSAEngineTest.kt
@@ -93,7 +93,7 @@ class X509EdDSAEngineTest {
     }
 
     /**
-     * Verify that signing with an X509Key wrapped EdDSA key succeeds when using the underlying EdDSAEngine.
+     * Verify that signing with an X509Key wrapped EdDSA key fails when using the underlying EdDSAEngine.
      */
     @Test
     fun `sign and verify with X509Key and old engine fails`() {
@@ -108,9 +108,11 @@ class X509EdDSAEngineTest {
 
         // Now verify the signature
         val signature = engine.sign()
-        engine.initVerify(publicKey)
-        engine.update(randomBytes)
-        engine.verify(signature)
+        assertFailsWith<InvalidKeyException> {
+            engine.initVerify(publicKey)
+            engine.update(randomBytes)
+            engine.verify(signature)
+        }
     }
 
     /** Verify will fail if the input public key cannot be converted to EdDSA public key. */
